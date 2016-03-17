259f14e3b24bf6445a2b3def8043b259cce4ad7c
zz <zz.at.field@gmail.com>
2016-03-17 21:23:34 +0800

    setup

--- setup.py
+++ setup.py
@@ -6,9 +6,33 @@ with open('README.rst', 'r', encoding='utf8') as f:
 
 version = __import__('esl_model').__version__
 
+
 setup(name='esl_model',
       version=version,
       description="Algorithm from The Elements of Statistical Learning book implement by Python code",
-      url=''
+      long_description=readme,
+      classifiers=[
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3 :: Only',
+          'Development Status :: 2 - Pre-Alpha',
+          'License :: OSI Approved :: MIT License',
+
+      ],
+      url='https://github.com/littlezz/ESL-Model',
+      author='littlezz',
+      author_email='zz.at.field@gmail.com',
+      license='MIT',
+      packages=['esl_model'],
+      install_requires=[
+            'numpy',
+            'pandas',
+            'scipy',
+            'sklearn',
+      ],
+      tests_require=['pytest'],
+      include_package_data=True,
+
+      zip_safe=False,
+
 
 )
